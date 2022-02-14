482b69fa61af7576dd6f153d5477630854d43f50
Suyeon04 <s2026@e-mirim.hs.kr>
2022-02-14 17:36:20 +0900

    ..

--- js/apply.js
+++ js/apply.js
@@ -1,6 +1,8 @@
 const comment = document.querySelector('.comment');
+const counter = document.querySelector('#counter');
 
 let wordlength = 0;
 comment.oninput = function(){
     wordlength = comment.value.length;
+    counter.innerText = "("+wordlength+"/500)";
 }
